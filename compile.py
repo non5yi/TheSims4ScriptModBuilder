e0511ad3c789923b1889d2f88cbf13290580e07d
Julie Jiang <hehejxw@163.com>
2024-11-24 14:46:52 +0800

    Update the project template for better usage
    In this change, modify several files to make it easier to run on
    windows system.
    1. Update all "/" seperators to os.path.join.
    2. Update decompile.py:
       a. makedirs for destination py files, otherwise it would fail
       on windows.
       b. add a successRate config option to make the script quit if
       decompile success rate < successRate%.
       c. Add a step copy_folders_to_src to copy important sims lib
       folders to src so that it's easier to import.
    3. Update compile.py:
       Add excusion folders in compile_py method so that sims lib will
       not be compiled into mod.
    4. helpers.injector.py:
       Got another injector file from modthesims forum.
    5. Update main.py to inject to SimSpawner.spawn_sim cls method and
       add some debug logs.

--- compile.py
+++ compile.py
@@ -12,12 +12,15 @@ from utils.utils import prepare_directory
 
 def compile_py(src: string, dest: string):
     # Compile the mod
-    for root, dirs, files in os.walk(src):
+    exclude = ("core", "lib", "simulation")
+    for root, dirs, files in os.walk(src, topdown=True):
+        dirs[:] = [d for d in dirs if d not in exclude]
         for filename in fnmatch.filter(files, '*.py'):
-            src_py = root + '/' + filename
+            src_py = os.path.join(root, filename)
             relative_path = str(Path(root).relative_to(src))
-            desc_path = dest + '/' + relative_path
-            compile_pyc = desc_path + '/' + filename.replace('.py', '.pyc')
+            dest_path = os.path.join(dest, relative_path)
+            compile_pyc = os.path.join(dest_path, filename.replace('.py', '.pyc'))
+            print("Compiling %s" % src_py)
 
             py_compile.compile(src_py, compile_pyc)
 
@@ -25,19 +28,21 @@ def compile_py(src: string, dest: string):
 def zip_ts4script(src: string):
     # zip
     zip_mod_file = mod_name + '.ts4script'
-    compiled_mod = PyZipFile(project_build_dir + '/' + zip_mod_file, mode='w', compression=ZIP_STORED, allowZip64=True,
-                             optimize=2)
+    compiled_mod = PyZipFile(os.path.join(project_build_dir, zip_mod_file),
+        mode='w', compression=ZIP_STORED, allowZip64=True,
+            optimize=2)
     for root, dirs, files in os.walk(src):
         relative_path = str(Path(root).relative_to(src))
         for file in files:
-            compiled_mod.write(os.path.join(root, file), relative_path + '/' + file)
+            compiled_mod.write(os.path.join(root, file),
+                               os.path.join(relative_path, file))
     compiled_mod.close()
 
     return zip_mod_file
 
 
 def start_compile():
-    my_game_mod_dir = mods_dir + '/' + mod_name
+    my_game_mod_dir = os.path.join(mods_dir, mod_name)
 
     prepare_directory(project_build_dir)
     prepare_directory(project_build_compile_dir)
@@ -47,7 +52,10 @@ def start_compile():
     zip_mod_file = zip_ts4script(project_build_compile_dir)
 
     # Copy it over to the mods folder
-    shutil.copyfile(project_build_dir + '/' + zip_mod_file, my_game_mod_dir + '/' + zip_mod_file)
+    shutil.copyfile(
+        os.path.join(project_build_dir, zip_mod_file),
+            os.path.join(my_game_mod_dir, zip_mod_file))
 
 
 start_compile()
+print("Compilation complete.")
--- src/main.py
+++ src/main.py
@@ -1,17 +1,44 @@
+from helpers import injector
+import services
 import sims4.commands
+import sims4.log
+from sims.sim_spawner import SimSpawner
+from sims.sim_info_types import Gender
 
+logger = sims4.log.Logger('julie', default_owner='julie')
 
-@sims4.commands.Command('hello', command_type=sims4.commands.CommandType.Live)
-def hello_world(_connection=None):
+# Not that when injecting to cls function, if cls is not used in that function,
+# it is not passed in *args.
+@injector.inject_to(SimSpawner, 'spawn_sim')
+def _override_spawn_sim(original, *args, **kwargs):
+    logger.info("julie enter")
+    result = original(*args, **kwargs)
+
+    try:
+        if result:
+            logger.info("julie, args {}, kwargs {}", args, kwargs)
+            if not args:
+                return result
+            sim_info = args[0]
+            logger.info("julie, sim_info {}", sim_info)
+
+    except Exception as e:
+        logger.exception(
+            "julie SimSpawner.spawn_sim injection failed to run, err {}",
+            str(e))
+
+    logger.info("julie end.")
+    return result
+
+@sims4.commands.Command('getpopulation', command_type=sims4.commands.CommandType.Live)
+def getpop(_connection=None):
     output = sims4.commands.CheatOutput(_connection)
-    output("Hello World")
-
-
-@sims4.commands.Command('cheats_help', command_type=sims4.commands.CommandType.Live)
-def hello_world(_connection=None):
-    output = sims4.commands.CheatOutput(_connection)
-    output("add_money <amount>: add money to your current sim")
-    output("remove_money <amount>: remove money from your current sim")
-    output("max_skill <skill name (no space)>: set the skill to max level")
-    output("become_friend <firstname> <lastname>: become friend with the target sim")
-    output("become_lover <firstname> <lastname>: become lover with the target sim")
+    allSims = services.sim_info_manager().get_all()
+    allCnt = len(allSims)
+    maleCnt = 0
+    for simInfo in allSims:
+        if (simInfo.gender == Gender.MALE):
+            maleCnt += 1
+            
+    output('Your town\'s population is {}, male {}, female {}.'.format(
+        allCnt, maleCnt, allCnt - maleCnt))
